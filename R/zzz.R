8cfdcb7dbfe881ee63314d966d3d0b0b313011c5
Kevin Reuning <kevin.reuning@gmail.com>
2020-07-29 16:51:21 -0400

    Adding Startup Message

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to the POL306 Tutorial")
+  packageStartupMessage("If there are no Tutorials listead in the Tutorial tab, close and reopen RStudio")
+}
